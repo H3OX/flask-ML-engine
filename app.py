12580b0169a33f0a0e9c3c2ecb9e1ee143012da8
H3OX <piankovg@gmail.com>
2019-10-02 17:35:14 +0300

    init2

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ import numpy as np
 app = Flask(__name__) 
 model = joblib.load('model.joblib')
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def predict():
-    data = request.get_json(force=True)
-    prediction = model.predict(np.array([data['params']]).tolist()).tolist()
-    output = prediction
-    return jsonify(output)
+    if request.method == 'POST':
+        data = request.get_json(force=True)
+        prediction = model.predict(np.array([data['params']]).tolist()).tolist()
+        output = prediction
+        return jsonify(output)
+    else:
+        return jsonify(err = 'err')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
